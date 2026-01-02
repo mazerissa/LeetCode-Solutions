69895dd106a9980f48d27eb2f5567caf56d15f9b
mazerissa <154016962+mazerissa@users.noreply.github.com>
2026-01-02 21:15:16 +0100

    Replace xrange with range for Python 3 compatibility
    ## Summary
    Fixed a NameError by changing `Xrange` to `range` throughout the solution.
    
    ## Issue
    The current code throws an error when executed:

--- Python/maximize-cyclic-partition-score.py
+++ Python/maximize-cyclic-partition-score.py
@@ -12,15 +12,15 @@ class Solution(object):
         def best_time_to_buy_and_sell_stock_v(base):
             dp = [0]*(len(nums)+1)
             result = 0
-            for i in xrange(k):
+            for i in range(k):
                 x, y = float("-inf"), float("-inf")
                 new_dp = [float("-inf")]*(len(nums)+1)
-                for j in xrange(i, len(nums)):
+                for j in range(i, len(nums)):
                     x, y = max(x, dp[j]-nums[(base+j)%len(nums)]), max(y, dp[j]+nums[(base+j)%len(nums)])
                     new_dp[j+1] = max(new_dp[j], x+nums[(base+j)%len(nums)], y-nums[(base+j)%len(nums)])
                 dp = new_dp
                 result = max(result, dp[-1])
             return result
     
-        i = min(xrange(len(nums)), key=lambda x: nums[x])
+        i = min(range(len(nums)), key=lambda x: nums[x])
         return max(best_time_to_buy_and_sell_stock_v(i), best_time_to_buy_and_sell_stock_v(i+1))
